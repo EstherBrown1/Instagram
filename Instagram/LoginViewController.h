a2cb9bbe606cda448d95bdf1613d18983a6e7e68
Esther Brown <estherb@fb.com>
2019-07-10 06:31:07 -0700

    Created SignUp View Controller for Storyboard

---
+++ Instagram/LoginViewController.h
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.h
+//  Instagram
+//
+//  Created by estherb on 7/9/19.
+//  Copyright © 2019 estherb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Parse/Parse.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LoginViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
