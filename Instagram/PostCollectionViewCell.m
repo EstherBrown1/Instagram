aac053b059915c4a1066fe4561d7707ff2ecc0ea
Esther Brown <estherb@fb.com>
2019-07-11 12:56:26 -0700

    ProfileViewController now shows up correctly

---
+++ Instagram/PostCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  PostCollectionViewCell.m
+//  Instagram
+//
+//  Created by estherb on 7/11/19.
+//  Copyright © 2019 estherb. All rights reserved.
+//
+
+#import "PostCollectionViewCell.h"
+
+@implementation PostCollectionViewCell
+
+@end
